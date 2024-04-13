d43df962f4f9ed98a04787beb689b6217fab8d57
Jang HuiSeon <xkzl8521@gmail.com>
2024-04-13 17:51:53 +0900

    Feat: 프로젝트 초기 세팅

---
+++ ToDoGeo/ToDoGeo/Presenter/Base/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  ToDoGeo
+//
+//  Created by SUN on 4/13/24.
+//
+
+import UIKit.UIViewController
+import class RxSwift.DisposeBag
+
+class BaseViewController: UIViewController {
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLayout()
+        setupAttribute()
+    }
+    
+    /// 레이아웃, addSubview 등
+    func setupLayout() {}
+    /// 각 UI의 특성을 추가
+    func setupAttribute() {}
+}
