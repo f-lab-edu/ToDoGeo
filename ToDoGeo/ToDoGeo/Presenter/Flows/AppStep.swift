9b33e3f2a5897d912cd2aeb96c82833c7961cbcc
Jang HuiSeon <xkzl8521@gmail.com>
2024-04-23 23:11:31 +0900

    Chore: AppStep 파일 주석 추가

--- ToDoGeo/ToDoGeo/Presenter/Flows/AppStep.swift
+++ ToDoGeo/ToDoGeo/Presenter/Flows/AppStep.swift
@@ -7,9 +7,13 @@
 
 import RxFlow
 
+/// 화면 이동 로직
 enum AppStep: Step {
+    /// sign In 화면으로 이동
     case signInRequired
+    /// sign up 화면으로 이동
     case signUpRequired
     
+    /// todo 화면으로 이동
     case toDoRequired
 }
