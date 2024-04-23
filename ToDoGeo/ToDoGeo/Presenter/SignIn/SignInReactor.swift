3cea2b7087c5c620b780b64d017f4b60b3d8b32c
Jang HuiSeon <xkzl8521@gmail.com>
2024-04-23 23:02:32 +0900

    chore: reactor Todo 주석 추가

--- ToDoGeo/ToDoGeo/Presenter/SignIn/SignInReactor.swift
+++ ToDoGeo/ToDoGeo/Presenter/SignIn/SignInReactor.swift
@@ -20,10 +20,10 @@ final class SignInReactor: Reactor, Stepper {
     }
     
     struct State {
-        
+        // TODO: 프로퍼티 추가 예정
     }
     
     enum Action {
-    
+        // TODO: 프로퍼티 추가 예정
     }
 }
