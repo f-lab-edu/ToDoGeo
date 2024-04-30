f773b16c614465a3daf42aba0d8b09ec293c6b32
Jang HuiSeon <xkzl8521@gmail.com>
2024-04-30 23:07:48 +0900

    Feat: Data 레이어에서 사용할 네트워크 레이어 추가

---
+++ ToDoGeo/ToDoGeo/Presenter/Flows/APIService.swift
@@ -0,0 +1,35 @@
+//
+//  APIService.swift
+//  ToDoGeo
+//
+//  Created by SUN on 4/30/24.
+//
+
+import Alamofire
+import RxSwift
+
+protocol APIServiceProtocol {
+    func request<Target: URLRequestConvertible, Response: Codable>(request: Target, response: Response.Type) -> Observable<Response>
+}
+
+final class APIService: APIServiceProtocol {
+    /// api request 함수
+    func request<Target: URLRequestConvertible, Response: Codable>(request: Target, response: Response.Type) -> Observable<Response> {
+        return Observable.create({ observer -> Disposable in
+            API.makeDataRequest(request)
+                .validate()
+                .responseDecodable(of: response.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        observer.onNext(data)
+                        
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                }
+            
+            return Disposables.create()
+        })
+    }
+}
+
