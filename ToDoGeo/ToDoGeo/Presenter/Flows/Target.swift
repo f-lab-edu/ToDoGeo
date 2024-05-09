8a43a66aa4479c1f98c0e88dc0be8995275403a0
Jang Huiseon <xkzl8521@gmail.com>
2024-05-09 11:41:57 +0900

    Refactor: Target의 내부 함수 및 구조 개선

--- ToDoGeo/ToDoGeo/Presenter/Flows/Target.swift
+++ ToDoGeo/ToDoGeo/Presenter/Flows/Target.swift
@@ -20,55 +20,51 @@ extension Target {
     func asURLRequest() throws -> URLRequest {
         let url = try baseURL.asURL()
         var urlRequest = try URLRequest(url: url.appendingPathComponent(path), method: method)
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.headers = HTTPHeaders(headers)
         
         switch parameter {
-        case .query(let query):
-            let queryToParameter = query?.toParameter()
-            let queryItems = queryToParameter?.compactMap({ URLQueryItem(name: $0.key, value: "\($0.value)") })
-            var components = URLComponents(string: url.appendingPathComponent(path).absoluteString)
-            components?.queryItems = queryItems
-            urlRequest.url = components?.url
-            
-        case .body(let body):
-            let parameter = body?.toParameter()
-            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameter ?? [:])
-            
         case .queryNBody(let query, let body):
-            let queryToParameter = query?.toParameter()
-            let queryItems = queryToParameter?.compactMap({ URLQueryItem(name: $0.key, value: "\($0.value)") })
-            var components = URLComponents(string: url.appendingPathComponent(path).absoluteString)
-            components?.queryItems = queryItems
-            urlRequest.url = components?.url
-            let parameter = body?.toParameter()
-            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameter ?? [:])
+            urlRequest.url = try makeUrlWithQuery(url: urlRequest.url, query: query)
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: body?.toParameter() ?? [:])
             
         case .none:
             break
         }
         return urlRequest
     }
+    
+    /// url에 쿼리 세팅
+    private func makeUrlWithQuery(url: URL?, query: Encodable?) throws -> URL? {
+        guard let url = url else {
+            return nil
+        }
+        let queryToParameter = try query?.toParameter()
+        let queryItems = queryToParameter?.compactMap({ URLQueryItem(name: $0.key, value: "\($0.value)") })
+        var components = URLComponents(string: url.absoluteString)
+        components?.queryItems = queryItems
+        
+        return components?.url
+    }
 }
 
 enum RequestParameter {
-    case query(_ query: Encodable?)
-    case body(_ parameter: Encodable?)
     case queryNBody(_ query: Encodable?, _ parameter: Encodable?)
     case none
 }
 
 extension Encodable {
-    func toParameter() -> Parameters {
-        do {
-            guard let data = try? JSONEncoder().encode(self),
-                  let parameter = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? Parameters else {
-                return Parameters()
-            }
-            return parameter
-        } catch(let error) {
-            debugPrint(String(format: "[Codable encodeJsonError: %@]", error.localizedDescription))
-            return Parameters()
+    func toParameter() throws -> Parameters {
+        let data = try JSONEncoder().encode(self)
+        let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+        
+        guard let parameter = jsonObject as? Parameters else {
+            throw SerializationError.invalidParameter
         }
+        
+        return parameter
     }
+}
+
+enum SerializationError: Error {
+    case invalidParameter
 }
