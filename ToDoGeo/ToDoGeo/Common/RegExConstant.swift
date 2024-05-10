50da22ea7b5303e94a5b1fcfb3269f0d6bdaa389
Jang HuiSeon <xkzl8521@gmail.com>
2024-05-10 15:55:35 +0900

    Fix: 비밀번호 정규식 변수명 변경

--- ToDoGeo/ToDoGeo/Common/RegExConstant.swift
+++ ToDoGeo/ToDoGeo/Common/RegExConstant.swift
@@ -11,6 +11,6 @@ enum RegExConstant {
     /// 이메일 유효성 체크 정규식
     static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
     /// 비밀번호 유효성 체크 정규식
-    static let password = "(?=.*[A-Za-z])(?=.*[0-9]).{8,20}"
+    static let pw = "(?=.*[A-Za-z])(?=.*[0-9]).{8,20}"
 
 }
