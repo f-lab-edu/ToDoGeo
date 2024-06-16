d40dae4f1a0376790175077a53241c772c7654d3
Jang HuiSeon <xkzl8521@gmail.com>
2024-06-16 15:02:40 +0900

    Feat: ToDo Entity 추가

---
+++ ToDoGeo/Entity/Todo/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoGeo
+//
+//  Created by SUN on 6/15/24.
+//
+
+import CoreLocation
+
+struct ToDo {
+    /// 제목
+    var title: String
+    /// 알림 받을 위치
+    var location: CLLocationCoordinate2D
+}
