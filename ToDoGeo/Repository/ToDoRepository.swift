6862c7eaecfb6633f47e1a5b89648f602590ca79
Jang Huiseon <xkzl8521@gmail.com>
2024-07-03 17:15:51 +0900

    Feat: ToDo Firebase에 업로드 기능 추가

--- ToDoGeo/Repository/ToDoRepository.swift
+++ ToDoGeo/Repository/ToDoRepository.swift
@@ -5,7 +5,8 @@
 //  Created by SUN on 7/2/24.
 //
 
-import Firebase
+import FirebaseAuth
+import FirebaseDatabaseInternal
 import RxSwift
 
 final class ToDoRepository: ToDoRepositoryProtocol {
@@ -13,6 +14,23 @@ final class ToDoRepository: ToDoRepositoryProtocol {
     /// - Parameter todo: 등록할 ToDo
     /// - Returns: 등록 결과
     func addToDo(_ todo: ToDo) -> Observable<Void> {
-        
+        Observable.create { observer -> Disposable in
+            guard let userId = Auth.auth().currentUser?.uid  else {
+                observer.onError(FireBaseAuthError.invalidUserId)
+                return Disposables.create()
+            }
+            
+            let ref = Database.database().reference().child("users").child(userId).child("todos").childByAutoId()
+            ref.setValue(todo.toDictionary()){
+                (error:Error?, ref:DatabaseReference) in
+                if let error = error {
+                    observer.onError(error)
+                } else {
+                    observer.onNext(())
+                }
+            }
+            
+            return Disposables.create()
+        }
     }
 }
---
+++ ToDoGeo/ToDoGeo/Common/Error/FireBaseAuthError.swift
@@ -0,0 +1,12 @@
+//
+//  FireBaseAuthError.swift
+//  ToDoGeo
+//
+//  Created by SUN on 7/3/24.
+//
+
+import Foundation
+
+enum FireBaseAuthError: Error {
+    case invalidUserId
+}
